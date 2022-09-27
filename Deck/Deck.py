dbc96f8b8cdd6a614750de2c90cf0c954edb7152
Alex <alja.platt@gmail.com>
2022-09-27 16:44:17 +0100

    Card class

---
+++ Deck/Deck.py
@@ -0,0 +1,6 @@
+# class Deck:
+#     def __init__(self, suits, ranks, cl):
+#         self.deck = []
+#         for suit in suits:
+#             for rank in ranks:
+#                 self.deck.append(cl(suit, rank))
